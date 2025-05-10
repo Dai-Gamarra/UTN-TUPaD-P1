c3b473f6ac23d52e66a4781ffdec7ae23f1366f1
Dai-Gaamarra <daiana.gamarra.cp@gmail.com>
2025-05-10 19:54:34 -0300

    respuestas TP Nº5

---
+++ 05 Funciones/Gamarra_TP-05.py
@@ -0,0 +1,17 @@
+#1 Crea una lista con los múltiplos de 4 entre 1 y 100 usando range
+multiplos_de_4 = list(range(4, 101, 4))  # Comienza en 4, termina en 100, de a saltos de 4
+print(multiplos_de_4)
+
+# 2Crear una lista con 5 elementos que te gusten
+mis_favoritos = ["Python", "Pizza", "Viajar", "Música", "Gatos"]
+# Mostrar el penúltimo elemento usando índice negativo
+print("El penúltimo elemento es:", mis_favoritos[-2])
+
+#3 Crear una lista vacía
+lista_vacia = []
+# Agregar tres palabras usando append
+lista_vacia.append("Sol")
+lista_vacia.append("Montaña")
+lista_vacia.append("Chocolate")
+# Mostrar la lista resultante
+print("Lista final:", lista_vacia)
